51217a722081ed22c8c3a3acb67d7fd82f00a6c4
tester test <test@test.com>
2022-01-28 13:03:45 -0500

    GammaController

---
+++ contracts/interfaces/gamma/IUniProxy.sol
@@ -0,0 +1,14 @@
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+interface IUniProxy {
+
+  function deposit(
+    uint256 deposit0,
+    uint256 deposit1,
+    address to,
+    address from,
+    address pos
+  ) external returns (uint256 shares);
+
+}
